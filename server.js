940919fd8ffa3abb8f64e5f61f345ad53c1443fa
adityatanwar <adityatanwar99@gmail.com>
2018-01-16 18:19:03 +0530

    add all the files

---
+++ server.js
@@ -0,0 +1,9 @@
+var express=require('express');
+var app=express();
+var port=3000 || process.env.PORT;
+app.get("/",function(req,res){
+    res.send("Hello from the backend")
+});
+app.listen(port,function(){
+    console.log('Server is running');
+});
